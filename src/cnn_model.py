37356c230eeb7a26d50c17aefb0fc6f1e01a6c6e
Alan Thomas <thomasalan535@gmail.com>
2024-04-14 22:12:54 +0530

    modifying cnn model

--- src/cnn_model.py
+++ src/cnn_model.py
@@ -6,7 +6,7 @@ import pickle
 from pickle_operations import *
 
 directory=r"C:\Users\LENOVO\OneDrive\Desktop\DEVSOC\NEW_AP\DATASET\data\train"
-pickle_path = r'C:\Users\LENOVO\Documents\Important_documents\VIT\HACK\DEVSOC\NEW_AP\DD\data\pickle'
+pickle_path = r'C:\Users\LENOVO\Documents\Important_documents\VIT\HACK\DEVSOC\NEW_AP\devsoc\pickled data'
 
 # X = pickle_read(pickle_path,'X.pickle')
 # y = pickle_read(pickle_path,'y.pickle')
@@ -23,29 +23,32 @@ try:
 except:
     print("File errors. Couldnt find files in the following directory - \n",os.path.join(pickle_path,pickle_path))
 
+X_normalised = np.array(X_normalised)
+y = np.array(y)
+
 print(np.shape(y))
 print(np.shape(X_normalised))
 
 input_shape = (1280, 720, 1)
 
-model = tf.keras.Sequential([
-tf.keras.layers.Input(shape=input_shape),
-tf.keras.layers.Conv2D(32, 3, activation='relu'),
-tf.keras.layers.BatchNormalization(),
-tf.keras.layers.MaxPooling2D(),
-tf.keras.layers.Conv2D(64, 3, activation='relu'),
-tf.keras.layers.MaxPooling2D(),
-tf.keras.layers.Conv2D(128, 3, activation='relu'),
-tf.keras.layers.MaxPooling2D(),
-tf.keras.layers.Flatten(),
-tf.keras.layers.Dense(256, activation='relu'),
-tf.keras.layers.Dense(2, activation= 'softmax')
-])
+# model = tf.keras.Sequential([
+# tf.keras.layers.Input(shape=input_shape),
+# tf.keras.layers.Conv2D(32, 3, activation='relu'),
+# tf.keras.layers.BatchNormalization(),
+# tf.keras.layers.MaxPooling2D(),
+# tf.keras.layers.Conv2D(64, 3, activation='relu'),
+# tf.keras.layers.MaxPooling2D(),
+# tf.keras.layers.Conv2D(128, 3, activation='relu'),
+# tf.keras.layers.MaxPooling2D(),
+# tf.keras.layers.Flatten(),
+# tf.keras.layers.Dense(256, activation='relu'),
+# tf.keras.layers.Dense(2, activation= 'softmax')
+# ])
 
-# print(model.summary())
+# # print(model.summary())
 
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])   #binary_cross_entropy because there are only two classes
+# model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])   #binary_cross_entropy because there are only two classes
 
-model.fit(X_normalised, y, epochs=10, validation_split=0.2)
+# model.fit(X_normalised, y, epochs=10, validation_split=0.2)
 
-model.save(os.path.join(pickle_path,'model.hd5'))
+# model.save(os.path.join(pickle_path,'model.hd5'))
